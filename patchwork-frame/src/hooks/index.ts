1cb0fe2e681b3979e931e0a3d43a499431168ba0
Orion Reed <orionreedt@gmail.com>
2026-06-02 19:44:26 +0200

    remove old exports from frame

--- patchwork-frame/src/hooks/index.ts
+++ patchwork-frame/src/hooks/index.ts
@@ -1,8 +1,6 @@
 export { useSidebarState } from "./useSidebarState";
 export { useSidebarResize } from "./useSidebarResize";
 export { useSelectedView as useSelectedDocument } from "./useSelectedDocument";
-export { useAnnotations } from "./useAnnotations";
-export { useCommentThreads } from "./useCommentThreads";
 export { useProviderReady } from "./useProviderReady";
 export {
   DebugRegistryToast,
