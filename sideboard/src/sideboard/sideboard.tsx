ad45431af0bddca90889a4a007ef4031e726271d
chee <yay@chee.party>
2026-06-30 11:11:50 +0100

    show loading rows?

--- sideboard/src/sideboard/sideboard.tsx
+++ sideboard/src/sideboard/sideboard.tsx
@@ -3,6 +3,7 @@ import { Show } from "solid-js";
 
 import type { PatchworkToolProps, SideboardAccountDoc } from "../types.ts";
 import { DocumentListPanel } from "./document-list-panel.tsx";
+import { LoadingRows } from "./document-list/loading-row.tsx";
 import { AccountBar } from "./account-bar.tsx";
 
 /**
@@ -14,7 +15,16 @@ export function Sideboard(props: PatchworkToolProps<SideboardAccountDoc>) {
 
   return (
     <div class="sideboard">
-      <Show when={doc.rootFolderUrl}>
+      <Show
+        when={doc.rootFolderUrl}
+        fallback={
+          <aside class="document-list">
+            <nav class="document-list__doclist document-list-widget">
+              <LoadingRows depth={0} />
+            </nav>
+          </aside>
+        }
+      >
         <DocumentListPanel
           folderUrl={doc.rootFolderUrl!}
           repo={props.repo}
